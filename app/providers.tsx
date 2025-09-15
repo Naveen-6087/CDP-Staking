d3b7dbb6f8335770250228e4552973c1f291e49c
Naveen-6087 <v.naveenc1a@gmail.com>
2025-09-16 02:56:47 +0530

    feat: Added connectkit and wagmi

---
+++ app/providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { hoodi } from 'wagmi/chains';
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [hoodi],
+    transports: {
+      [hoodi.id]: http(`https://ethereum-hoodi-rpc.publicnode.com`),
+    },
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    appName: "Staking Tutorial",
+  }),
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>
+          {children}
+        </ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
